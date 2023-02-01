e15f28ea6d7b3a5139864dad7f9425e61518dbde
Aaryaman Sharma <31642776+Aaryaman09@users.noreply.github.com>
2023-02-02 02:16:33 +0530

    Template Repo Completed

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, World! I Hope this is working"
+    return "Hi, This is the template repo"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5050)
